9eb46f607c8eaba676850511a1da316b25f56709
edsoncarlosdvp <edsoncarlos@equipenovainfo.com.br>
2021-01-12 20:42:03 -0300

    Intervalo de aula.

---
+++ webfullstack/aula14/js/script.js
@@ -0,0 +1,13 @@
+function somar() {
+    var numero1 = 10
+    var numero2 = 20
+    var resultado = numero1 + numero2
+
+    console.log("O resultado da soma é: " + resultado)
+}
+
+somar(10, 40)
+
+function boasVindas(nome) {
+    alert("Seja bem-vindo, você pressionou  o botão cinza!")
+}
