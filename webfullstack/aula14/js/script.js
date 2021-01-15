f3f3b1c610db779c0e8d7e7814116003e926e179
edsoncarlosdvp <edsoncarlos@equipenovainfo.com.br>
2021-01-14 22:30:10 -0300

    Calculadora concluída!

--- webfullstack/aula14/js/script.js
+++ webfullstack/aula14/js/script.js
@@ -1,7 +1,7 @@
 function somar() {
     var numero1 = 10
     var numero2 = 20
-    var resultado = numero1 + numero2
+    resultado = numero1 + numero2
 
     console.log("O resultado da soma é: " + resultado)
 }
@@ -18,4 +18,54 @@ function boasVindasLog() {
 
 function verificaMudançadeTexto() {
     alert("Seja bem-vindo, houve uma mudança!")
+}
+
+// CALCULADORA
+
+function limparParcial() {
+    var num1 = document.querySelector("#num1").value = ""
+    var num2 = document.querySelector("#num2").value = ""
+    var opc = document.querySelector("#operacao").value = ""
+}
+
+function limpar() {
+    var num1 = document.querySelector("#num1").value = ""
+    var num2 = document.querySelector("#num2").value = ""
+    var opc = document.querySelector("#operacao").value = ""
+    document.querySelector("#resultado").value = ""
+}
+
+function calcular() {
+    var num1 = document.querySelector("#num1").value
+    var num2 = document.querySelector("#num2").value
+    var opc = document.querySelector("#operacao").value
+    var res
+
+    if (num1 == "" || num2 == "") {
+        alert("Digite os valores para efetuar o cálculo.")
+    } else {
+
+        switch (opc) {
+            case "adi":
+                res = Number(num1) + Number(num2)
+                break
+            case "sub":
+                res = Number(num1) - Number(num2)
+                break
+            case "mult":
+                res = Number(num1) * Number(num2)
+                break
+            case "div":
+                if (num2 == 0) {
+                    alert("Não divisível por 0");
+                    return false;
+                }
+                res = Number(num1) / Number(num2)
+                break
+            default:
+                alert("Escolha uma operação válida!")
+        }
+        limparParcial()
+        document.querySelector("#resultado").value = res
+    }
 }
