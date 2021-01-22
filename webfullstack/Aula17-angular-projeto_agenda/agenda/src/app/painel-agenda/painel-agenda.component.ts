22f372bece10ffa851320a8dee553055ee78b5fd
edsoncarlosdvp <edsoncarlos@equipenovainfo.com.br>
2021-01-21 22:39:20 -0300

    Aula17 - Angular => Projeto Agenda

---
+++ webfullstack/Aula17-angular-projeto_agenda/agenda/src/app/painel-agenda/painel-agenda.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-painel-agenda',
+  templateUrl: './painel-agenda.component.html',
+  styleUrls: ['./painel-agenda.component.css']
+})
+export class PainelAgendaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
