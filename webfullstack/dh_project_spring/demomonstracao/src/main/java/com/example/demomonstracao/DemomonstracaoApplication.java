016885e997c3b3c4383b9d65bd3f35122621f3ba
Edson Carlos <edsoncarlos@equipenovainfo.com.br>
2021-03-30 22:39:24 -0300

    Atualizando reenvio e iniciando aula com Spring Boot

---
+++ webfullstack/dh_project_spring/demomonstracao/src/main/java/com/example/demomonstracao/DemomonstracaoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demomonstracao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemomonstracaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemomonstracaoApplication.class, args);
+	}
+
+}
---
+++ webfullstack/dh_project_spring/demomonstracao/src/test/java/com/example/demomonstracao/DemomonstracaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demomonstracao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemomonstracaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
